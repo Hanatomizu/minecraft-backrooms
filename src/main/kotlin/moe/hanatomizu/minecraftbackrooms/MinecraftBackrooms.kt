a4dffc371ae5e80687f07e66ea09ea7bb75c9adf
Hanatomizu <chart11from21@outlook.com>
2024-03-01 20:48:42 +0800

    feat: add basic data generator
    Signed-off-by: Hanatomizu <chart11from21@outlook.com>

--- src/main/kotlin/moe/hanatomizu/minecraftbackrooms/MinecraftBackrooms.kt
+++ src/main/kotlin/moe/hanatomizu/minecraftbackrooms/MinecraftBackrooms.kt
@@ -17,8 +17,10 @@
 package moe.hanatomizu.minecraftbackrooms
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.item.BlockItem
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
@@ -33,6 +35,7 @@ class MinecraftBackrooms : ModInitializer {
 
         // Register blocks
         Registry.register(Registries.BLOCK, Identifier(NAMESPACE, "entrance_floor"), ENTRANCE_FLOOR)
+        Registry.register(Registries.ITEM, Identifier(NAMESPACE, "entrance_floor"), BlockItem(ENTRANCE_FLOOR, FabricItemSettings()))
 
     }
 }
