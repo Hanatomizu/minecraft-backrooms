1f42f44f7424fc6e9f2992d5b9d397df33e9a70c
Hanatomizu <chart11from21@outlook.com>
2024-03-03 00:03:41 +0800

    feat: add item group

--- src/main/kotlin/moe/hanatomizu/minecraftbackrooms/MinecraftBackrooms.kt
+++ src/main/kotlin/moe/hanatomizu/minecraftbackrooms/MinecraftBackrooms.kt
@@ -16,14 +16,43 @@
 
 package moe.hanatomizu.minecraftbackrooms
 
+import moe.hanatomizu.minecraftbackrooms.objects.ModBlockItems
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+
 
 class MinecraftBackrooms : ModInitializer {
 
     private val NAMESPACE = "backrooms"
-    // Add blocks
+
+    // add Item Groups
+    val ENTRANCE_BLOCKS: RegistryKey<ItemGroup> = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier(NAMESPACE, "entrance_blocks"))
 
     override fun onInitialize (){
+        // Register Item Groups
+        Registry.register(Registries.ITEM_GROUP, ENTRANCE_BLOCKS, FabricItemGroup.builder()
+            .icon{ ItemStack(ModBlockItems.ENTRANCE_WALL_ITEM) }
+            .displayName(Text.translatable("itemGroup.backrooms.entrance_blocks"))
+            .build()
+        )
+
+        // Add Items into item groups
+        ItemGroupEvents.modifyEntriesEvent(ENTRANCE_BLOCKS)
+            .register(ModifyEntries { content: FabricItemGroupEntries ->
+                content.add(ModBlockItems.ENTRANCE_FLOOR_ITEM)
+                content.add(ModBlockItems.ENTRANCE_WALL_ITEM)
+            })
 
         // Register blocks and block items
         moe.hanatomizu.minecraftbackrooms.registry.Blocks.init()
